9c69bc2a1f24e26decd8348488215bdd181d5cbe
UTP <UTP@iMac-de-Administrator.local>
2024-07-15 16:09:35 -0500

    Chat implemented with message persistance

--- server/index.js
+++ server/index.js
@@ -1,16 +1,79 @@
 import express from "express";
 import logger from "morgan";
+import dotenv from "dotenv";
+import { createClient } from "@libsql/client";
+
+import { Server } from "socket.io";
+import { createServer } from "http";
+
+dotenv.config();
 
 const port = process.env.PORT ?? 3000;
 
 const app = express();
+const server = createServer(app);
+const io = new Server(server, {
+  connectionStateRecovery: {
+    connectionStateRecovery: {},
+  },
+});
+
+const db = createClient({
+  url: "libsql://striking-demogoblin-mateomor.turso.io",
+  authToken: process.env.DB_TOKEN,
+});
+
+await db.execute(` CREATE TABLE IF NOT EXISTS messages (
+  id INTEGER PRIMARY KEY AUTOINCREMENT,
+  content TEXT
+)`);
+
+io.on("connection", async (socket) => {
+  console.log("user connected");
+
+  socket.on("disconnect", () => {
+    console.log("user disconnected");
+  });
+
+  socket.on("chat message", async (msg) => {
+    let result;
+    try {
+      result = await db.execute({
+        sql: "INSERT INTO messages (content) VALUES (:msg)",
+        args: { msg },
+      });
+    } catch (e) {
+      console.error(e);
+      return;
+    }
+
+    io.emit("chat message", msg, result.lastInsertRowid.toString());
+  }); 
+
+  if(!socket.recovered){ // Recuperar los mesnsajes sin conexion
+    try {
+      const results = await db.execute({
+        sql: "SELECT id, content FROM messages WHERE id > ?",
+        args: [socket.handshake.auth.serverOffset ?? 0],
+      })
+
+      results.rows.forEach((row) => {
+        socket.emit("chat message", row.content, row.id.toString());
+      })
+    } catch (error) {
+      console.error(error);
+    }
+  }
+
+});
+
 
 app.use(logger("dev"));
 
 app.get("/", (req, res) => {
-  res.send("<h1>Hello World</h1>");
+  res.sendFile(process.cwd() + "/client/index.html");
 });
 
-app.listen(port, () => {
+server.listen(port, () => {
   console.log(`Listening on port ${port}`);
 });
